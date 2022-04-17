599436ff50b10e72e7bfbcf8f4686e259081d54b
georgeouzou <geothrock@gmail.com>
2022-04-18 00:18:16 +0300

    add more tests for approximate transforms

--- transform/tests.py
+++ transform/tests.py
@@ -14,6 +14,12 @@ def decdeg2dms(dd):
     degrees = degrees if is_positive else -degrees
     return (degrees,minutes,seconds)
 
+def round_list(l, dec):
+    return [round(i, dec) for i in l]
+
+def get_column(l2d, column):
+    return list(zip(*l2d))[column]
+
 class TransformAPITest(TestCase):
 
     def test_transformer_from_htrs07_tm07_to_hgrs87_tm87(self):
@@ -119,7 +125,6 @@ class TransformAPITest(TestCase):
         N = [16209.281, 16803.183, 17432.541, 18089.393, 20388.192, 21378.062]
         x, y = t(E, N)
         self.assertEqual(list(round(v, 3) for v in x), [368236.734, 362539.287, 364326.931, 362251.821, 362755.013, 365092.002])
-        #self.assertEqual(list(round(v, 3) for v in y), [4306147.681, 4306837.383, 4307436.674, 4308128.472, 4310418.872, 4311369.406])
 
         # Kotsakis
         xt = [
@@ -213,31 +218,256 @@ class TransformAPITest(TestCase):
         self.assertEqual(list(round(v, 3) for v in x), xt)
         self.assertEqual(list(round(v, 3) for v in y), yt)
     
-    def test_approximate_transforms(self):
-        # hatt - wgs84
-        x = [-10157.950, -16090.967, -2162.917]
-        y = [-21121.093, -19478.049, -19596.748]
+    def test_transformer_approximate_between_hatt_wgs84(self):
+        p0 = [
+            [-10157.950, -21121.093],
+            [-16090.967, -19478.049],
+            [-2162.917,  -19596.748],
+        ]
+        p1 = [
+            [22.348427556,40.560454368],
+            [22.278332446,40.575158532],
+            [22.442825451,40.574237399],
+        ]
 
         params = {
-            'from_srid': 1000000, # hatt
-            'to_srid': 4326,      # wgs84
-            'from_hatt_id': '27' # Alexandria
+            'from_hatt_id': '27',  # Alexandria
+            'to_srid': 4326,       # wgs84
         }
         t = WorkHorseTransformer(**params)
+        x, y = (get_column(p0, 0), get_column(p0, 1))
         lam, phi = t(x, y)
-        self.assertEqual(list(round(v, 6) for v in lam), [22.348428, 22.278332, 22.442825])
-        self.assertEqual(list(round(v, 6) for v in phi), [40.560454, 40.575159, 40.574237])
+        # round to 5 decimals
+        self.assertEqual(round_list(lam, 5), round_list(get_column(p1, 0), 5))
+        self.assertEqual(round_list(phi, 5), round_list(get_column(p1, 1), 5))
 
-        # hatt - ed50
+        # inverse
         params = {
-            'from_srid': 1000000, # hatt
+            'from_srid': 4326,   # wgs84
+            'to_hatt_id': '27' # Alexandria
+        }
+        t = WorkHorseTransformer(**params)
+        lam, phi = (get_column(p1, 0), get_column(p1, 1))
+        x, y = t(lam, phi)
+        # round to 1 meter
+        self.assertEqual(round_list(x, 0), round_list(get_column(p0, 0), 0))
+        self.assertEqual(round_list(y, 0), round_list(get_column(p0, 1), 0))
+
+    def test_transformer_approximate_between_hatt_ed50(self):
+        p0 = [
+            [-10157.950, -21121.093],
+            [-16090.967, -19478.049],
+            [-2162.917,  -19596.748],
+        ]
+        p1 = [
+            [22.349039642, 40.561875209],
+            [22.278945936, 40.576579407],
+            [22.443435951, 40.575657423],
+        ]
+
+        params = {
+            'from_hatt_id': '27', # Alexandria
             'to_srid': 4230,      # ed50
-            'from_hatt_id': '27' # Alexandria
         }
         t = WorkHorseTransformer(**params)
+        x, y = (get_column(p0, 0), get_column(p0, 1))
         lam, phi = t(x, y)
-        self.assertEqual(list(round(v, 6) for v in lam), [22.349040, 22.278946, 22.443436])
-        self.assertEqual(list(round(v, 6) for v in phi), [40.561875, 40.576579, 40.575657])
+        # round to 5 decimals
+        self.assertEqual(round_list(lam, 5), round_list(get_column(p1, 0), 5))
+        self.assertEqual(round_list(phi, 5), round_list(get_column(p1, 1), 5))
+
+        # inverse
+        params = {
+            'from_srid': 4230,      # ed50
+            'to_hatt_id': '27', # Alexandria
+        }
+        t = WorkHorseTransformer(**params)
+        lam, phi = (get_column(p1, 0), get_column(p1, 1))
+        x, y = t(lam, phi)
+        # round to 1 meter
+        self.assertEqual(round_list(x, 0), round_list(get_column(p0, 0), 0))
+        self.assertEqual(round_list(y, 0), round_list(get_column(p0, 1), 0))
+
+    def test_transformer_approximate_between_hgrs87_wgs84(self):
+        p0 = [
+            [273069.315, 4171340.811],
+            [360028.79, 4490989.86],
+            [354126.16, 4492735.79],
+            [368047.90, 4492374.34],
+            [357863.95, 4493264.94],
+            [357133.34, 4494124.95],
+            [370573.61, 4493899.90],
+            [359019.22, 4494659.98],
+            [361255.37, 4494828.49],
+            [366895.56, 4494897.20],
+            [356412.02, 4496626.27],
+            [365136.21, 4497187.49],
+            [361166.95, 4497382.23],
+            [357953.56, 4497550.05],
+            [353310.92, 4497950.95],
+            [367458.81, 4497850.08],
+        ]
+        p1 = [
+            [21.428909993,37.663839993],
+            [22.348427505,40.560454352],
+            [22.278332403,40.575158531],
+            [22.442825432,40.574237378],
+            [22.322355229,40.580573092],
+            [22.313531731,40.588191744],
+            [22.472342395,40.588374302],
+            [22.335688331,40.593332843],
+            [22.362067124,40.595227965],
+            [22.428684375,40.596772539],
+            [22.304442449,40.610591026],
+            [22.407409741,40.617112627],
+            [22.360460575,40.618209822],
+            [22.322449041,40.619175600],
+            [22.267492725,40.621975463],
+            [22.434718217,40.623454808],
+        ]
+        params = {
+            'from_srid' : 2100, # hgrs87
+            'to_srid': 4326, # wgs84
+        }
+        t = WorkHorseTransformer(**params)
+        x, y = (get_column(p0, 0), get_column(p0, 1))
+        lam, phi = t(x, y)
+        # round to 5 decimals
+        self.assertEqual(round_list(lam, 5), round_list(get_column(p1, 0), 5))
+        self.assertEqual(round_list(phi, 5), round_list(get_column(p1, 1), 5))
+
+        params = {
+            'from_srid': 4326, # wgs84
+            'to_srid' : 2100, # hgrs87
+        }
+        lam, phi = (get_column(p1, 0), get_column(p1, 1))
+        t = WorkHorseTransformer(**params)
+        x, y = t(lam, phi)
+        # round to 1 meter
+        self.assertEqual(round_list(x, 0), round_list(get_column(p0, 0), 0))
+        self.assertEqual(round_list(y, 0), round_list(get_column(p0, 1), 0))
+
+    def test_transformer_approximate_between_hgrs87_ed50(self):
+        p0 = [
+            [273069.315, 4171340.811],
+            [360028.79, 4490989.86],
+            [354126.16, 4492735.79],
+            [368047.90, 4492374.34],
+            [357863.95, 4493264.94],
+            [357133.34, 4494124.95],
+            [370573.61, 4493899.90],
+            [359019.22, 4494659.98],
+            [361255.37, 4494828.49],
+            [366895.56, 4494897.20],
+            [356412.02, 4496626.27],
+            [365136.21, 4497187.49],
+            [361166.95, 4497382.23],
+            [357953.56, 4497550.05],
+            [353310.92, 4497950.95],
+            [367458.81, 4497850.08],
+        ]
+        p1 = [
+            [21.429513438,37.665327202],
+            [22.349039591,40.561875193],
+            [22.278945893,40.576579406],
+            [22.443435932,40.575657402],
+            [22.322967971,40.581993609],
+            [22.314144702,40.589612132],
+            [22.472952487,40.589793845],
+            [22.336300948,40.594752995],
+            [22.362679280,40.596647933],
+            [22.429295336,40.598192119],
+            [22.30505579, 40.612010946],
+            [22.408021274,40.618531850],
+            [22.361072969,40.619629269],
+            [22.323062134,40.620595226],
+            [22.26810684, 40.623395317],
+            [22.435329312,40.624873740],
+        ]
+
+        params = {
+            'from_srid' : 2100, # hgrs87
+            'to_srid': 4230, # ed50
+        }
+        t = WorkHorseTransformer(**params)
+        x, y = (get_column(p0, 0), get_column(p0, 1))
+        lam, phi = t(x, y)
+        # round to 5 decimals
+        self.assertEqual(round_list(lam, 5), round_list(get_column(p1, 0), 5))
+        self.assertEqual(round_list(phi, 5), round_list(get_column(p1, 1), 5))
+
+        params = {
+            'from_srid': 4230, # ed50
+            'to_srid' : 2100, # hgrs87
+        }
+        lam, phi = (get_column(p1, 0), get_column(p1, 1))
+        t = WorkHorseTransformer(**params)
+        x, y = t(lam, phi)
+        # round to 1 meter
+        self.assertEqual(round_list(x, 0), round_list(get_column(p0, 0), 0))
+        self.assertEqual(round_list(y, 0), round_list(get_column(p0, 1), 0))
+
+    def test_transformer_approximate_between_hgrs87_ed50_utm34N(self):
+            p0 = [
+                [273069.315, 4171340.811],
+                [360028.79, 4490989.86],
+                [354126.16, 4492735.79],
+                [368047.90, 4492374.34],
+                [357863.95, 4493264.94],
+                [357133.34, 4494124.95],
+                [370573.61, 4493899.90],
+                [359019.22, 4494659.98],
+                [361255.37, 4494828.49],
+                [366895.56, 4494897.20],
+                [356412.02, 4496626.27],
+                [365136.21, 4497187.49],
+                [361166.95, 4497382.23],
+                [357953.56, 4497550.05],
+                [353310.92, 4497950.95],
+                [367458.81, 4497850.08],
+            ]
+            p1 = [
+                [537882.645,4168838.948],
+                [614211.067,4491075.004],
+                [608253.005,4492618.785],
+                [622177.941,4492731.634],
+                [611970.210,4493274.855],
+                [611210.804,4494109.400],
+                [624650.169,4494342.295],
+                [613077.189,4494708.307],
+                [615306.120,4494952.881],
+                [620940.379,4495213.693],
+                [610404.761,4496584.439],
+                [619104.086,4497442.601],
+                [615130.742,4497501.935],
+                [611913.771,4497560.125],
+                [607260.672,4497802.515],
+                [621402.654,4498183.943],
+            ]
+
+            params = {
+                'from_srid' : 2100, # hgrs87
+                'to_srid': 23034, # ed50, utm34N
+            }
+            t = WorkHorseTransformer(**params)
+            x, y = (get_column(p0, 0), get_column(p0, 1))
+            xr, yr  = t(x, y)
+            # round to 1 meter
+            self.assertEqual(round_list(xr, 0), round_list(get_column(p1, 0), 0))
+            self.assertEqual(round_list(yr, 0), round_list(get_column(p1, 1), 0))
+
+            params = {
+                'from_srid': 23034, # ed50, utm34N
+                'to_srid' : 2100, # hgrs87
+            }
+            x, y = (get_column(p1, 0), get_column(p1, 1))
+            t = WorkHorseTransformer(**params)
+            xr, yr = t(x, y)
+            # round to 1 meter
+            self.assertEqual(round_list(xr, 0), round_list(get_column(p0, 0), 0))
+            self.assertEqual(round_list(yr, 0), round_list(get_column(p0, 1), 0))
+
+
 
     def test_transformer_compile(self):
         HATT_SRID = 1000000
@@ -254,8 +484,6 @@ class TransformAPITest(TestCase):
             horse = WorkHorseTransformer(from_srid=GGRS_SRID, to_srid=HATT_SRID)
 
         horse = WorkHorseTransformer(from_srid=GGRS_SRID, to_srid=HATT_SRID, to_hatt_id=2)
-
-
 
     # def test_transform_features(self):
     #       params = {
