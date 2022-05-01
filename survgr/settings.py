79a8cd62e8c84e3d0ddd7ea598172e1f2b35d589
georgeouzou <geothrock@gmail.com>
2022-05-01 15:20:47 +0300

    automate debugging

--- survgr/settings.py
+++ survgr/settings.py
@@ -26,7 +26,7 @@ if (os.path.isfile(dotenv_file)) :
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get("SURVGR_DEBUG", False)
 
 ALLOWED_HOSTS = [
         'localhost',
