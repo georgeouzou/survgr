2dfc5d64401f5bcfeb5064d0b054868fda036581
georgeouzou <geothrock@gmail.com>
2022-05-14 12:35:40 +0300

    frontend: Use openlayers as recommended
    - only for transform page
    - fix animations
    - fix deprecated openlayers api usage

---
+++ frontend/src/scripts/hattblock_map.js
@@ -0,0 +1,224 @@
+// ol modules
+import 'ol/ol.css';
+import { Map, View } from 'ol';
+
+import { 
+    Vector as VectorSource, 
+    OSM as OSMSource 
+} from 'ol/source';
+
+import { 
+    VectorImage as VectorImageLayer, 
+    Vector as VectorLayer, 
+    Tile as TileLayer
+} from 'ol/layer';
+
+import GeoJSON from 'ol/format/GeoJSON';
+import { Style, Stroke, Fill, Text } from 'ol/style';
+import { transform as ol_transform } from 'ol/proj';
+import { Select, defaults as get_interaction_defaults, } from 'ol/interaction';
+import { getCenter as extent_get_center } from 'ol/extent';
+
+// hattblocks url
+import HATTBLOCK_FEATURES_URL from '../assets/hattblocks.min.geojson';
+
+// constants & globals
+const NOT_SELECTED_ID = -1;
+
+const GEOMETRY_STYLE = new Style({
+    fill: new Fill({
+        color: 'rgba(255, 255, 255, 0.6)',
+    }),
+    stroke: new Stroke({
+        color: '#319FD3',
+        width: 1,
+    }),
+});
+
+const GEOMETRY_SELECTED_STYLE = new Style({
+    fill: new Fill({
+        color: 'rgba(255, 255, 255, 0.8)',
+    }),
+    stroke: new Stroke({
+        color: '#319FD3',
+        width: 3,
+    }),
+});
+
+let LABEL_STYLE = new Style({
+    text: new Text({
+        font: '13px Calibri,sans-serif',
+        fill: new Fill({
+            color: '#000',
+        }),
+        overflow: true,
+    }),
+});
+
+let FEATURE_STYLE = [GEOMETRY_STYLE, LABEL_STYLE];
+let FEATURE_SELECTED_STYLE = [GEOMETRY_SELECTED_STYLE, LABEL_STYLE];
+
+export function init_map() {
+    // Map initialization.
+    // Uses a single map instance enabling the user to select graphically 
+    // the hatt block of choice. The map is contained within a modal and 
+    // the modal is related/bound on the show event to the calling html 
+    // container class "hatt-selection"
+
+    // this is the geojson source used in the map
+    let hattblocks = new VectorSource({
+        url: HATTBLOCK_FEATURES_URL,
+        format: new GeoJSON({
+            dataProjection: 'EPSG:4326'
+        }),
+    });
+    let view = new View({
+        projection: 'EPSG:3857', // spherical merc 
+        center: ol_transform([25.0,38.4],'EPSG:4326','EPSG:3857'),
+        zoom: 6,
+        maxZoom: 10
+    });
+
+    let only_blocks_layer = new VectorImageLayer({
+        source: hattblocks,
+        maxZoom: 7,
+    });
+
+    let detailed_blocks_layer = new VectorLayer({
+        source: hattblocks,
+        minZoom: 7,
+        declutter: true,
+        style: function (feature, resolution) {
+            LABEL_STYLE
+                .getText()
+                .setText(get_hattblock_label(feature, resolution));
+            return FEATURE_STYLE;
+        },
+    });
+
+    let default_interactions_list = get_interaction_defaults({altShiftDragRotate:false, pinchRotate:false});
+
+    let map = new Map({
+        layers:[
+            new TileLayer({
+                source: new OSMSource()
+            }),
+            only_blocks_layer,
+            detailed_blocks_layer,
+        ],
+        target: "map",
+        view: view,
+        interactions: default_interactions_list,
+    });
+
+    let select_action = new Select({
+        style: function (feature, resolution) {
+            LABEL_STYLE 
+                .getText()
+                .setText(get_hattblock_label(feature, resolution));
+            return FEATURE_SELECTED_STYLE;
+        }
+    });
+    
+    map.addInteraction(select_action);
+    // on select feature
+    select_action.getFeatures().on('add', (e) => {
+        let feature = e.element;
+        let name = feature.get('name');
+        $('#selected-feature-name').html('Επιλογή φύλλου χάρτη: ' + name);
+    });
+    // on unselect feature
+    select_action.getFeatures().on('remove', () => {
+        $('#selected-feature-name').html('Επιλογή φύλλου χάρτη: ');
+    });
+
+    // Modal events
+    var bound_hatt;
+
+    $('#map-modal').on('show.bs.modal', (e) => {
+        // get the related hatt container element and store it to the modal instance
+        bound_hatt = $(e.relatedTarget).parents('.hatt-selection');
+        // clear previous selected feature if exists
+        let selected_features = select_action.getFeatures();
+        selected_features.clear();
+        // and select again based on related id value
+        let id = parseInt(bound_hatt.find('.hatt-id').val());  
+        if (id != NOT_SELECTED_ID){
+            if (hattblocks.getFeatures().length == 0){
+                // CAUTION: the source might not have loaded the features the first time we open the map so...
+                hattblocks.on('featuresloadend', () => {
+                    selected_features.push(hattblocks.getFeatureById(id));
+                });
+            } else {
+                selected_features.push(hattblocks.getFeatureById(id));       
+            }
+        }
+    });
+
+    $('#map-modal').on('shown.bs.modal', (e) => {
+        // this is needed because i use the map inside of a modal
+        map.updateSize();
+        // pan view to the selected feature
+        if (select_action.getFeatures().getLength() > 0){
+            // get xmin, ymin, xmax, ymax of selected feature
+            let center = extent_get_center(select_action.getFeatures().item(0).getGeometry().getExtent());
+            view.animate({
+                center: center
+            });
+        } 
+    });
+
+    $('#map-modal').on('hide.bs.modal', (e) => {
+        // update the selected hatt name and hatt id elements
+        if (select_action.getFeatures().getLength() > 0){
+            let selected_feature = select_action.getFeatures().pop();
+            bound_hatt.find('.hatt-id').val(selected_feature.getId());
+            bound_hatt.find('.hatt-name').typeahead('val', selected_feature.get('name'));
+        } else {
+            bound_hatt.find('.hatt-id').val(NOT_SELECTED_ID);
+            bound_hatt.find('.hatt-name').typeahead('val', '');
+        }
+    });
+}
+
+// Utility
+
+function decdeg2dms(dd) {
+    let is_positive = dd >= 0;
+    dd = Math.abs(dd);
+    let seconds = dd*3600.0;
+    let minutes = Math.floor(seconds/60.0);
+    seconds = seconds % 60.0
+    let degrees = Math.floor(minutes/60.0)
+    minutes = minutes % 60.0
+    let sign = is_positive ? '' :  '-';
+    return [sign,degrees,minutes,seconds];
+}
+
+function get_hattblock_label(feature, resolution) {
+    let name = feature.get('name');
+    let okxe_id = feature.get('okxe_id');
+
+    if (resolution > 400) {
+        return [`#${okxe_id}`, ''];
+    }
+
+    let phi = feature.get('cy');
+    let lambda = feature.get('cx');
+    let [sign_phi, d_phi, m_phi, s_phi] = decdeg2dms(phi);
+    let [sign_lambda, d_lambda, m_lambda, s_lambda] = decdeg2dms(lambda);
+
+
+    let info1 = ` Φo ${sign_phi}${d_phi}\u00B0${m_phi}`;
+    let info2 = ` Λo ${sign_lambda}${d_lambda}\u00B0${m_lambda}'`;
+
+    return [
+        `#${okxe_id}`, '',
+        '\n', '',
+        ` ${name}`, '',
+        '\n', '',
+        info1, 'italic 11px Calibri, sans-serif',
+        '\n', '',
+        info2, 'italic 11px Calibri, sans-serif',
+    ];
+}
--- frontend/src/scripts/transform.js
+++ frontend/src/scripts/transform.js
@@ -1,5 +1,6 @@
 import '../styles/transform.css';
 import HATTBLOCK_FEATURES_URL from '../assets/hattblocks.min.geojson';
+import { init_map } from './hattblock_map.js';
 
 /*
  * Constants
@@ -73,178 +74,11 @@ function initBloodhound(){
   });
 }
 
-const geometryStyle = new ol.style.Style({
-    fill: new ol.style.Fill({
-        color: 'rgba(255, 255, 255, 0.6)',
-    }),
-    stroke: new ol.style.Stroke({
-        color: '#319FD3',
-        width: 1,
-    }),
-});
-
-const geometrySelectedStyle = new ol.style.Style({
-    fill: new ol.style.Fill({
-        color: 'rgba(255, 255, 255, 0.8)',
-    }),
-    stroke: new ol.style.Stroke({
-        color: '#319FD3',
-        width: 3,
-    }),
-});
-
-const labelStyle = new ol.style.Style({
-    text: new ol.style.Text({
-        font: '13px Calibri,sans-serif',
-        fill: new ol.style.Fill({
-            color: '#000',
-        }),
-        overflow: true,
-    }),
-});
-
-const featureStyle = [geometryStyle, labelStyle];
-const featureSelectedStyle = [geometrySelectedStyle, labelStyle];
-
-function initMap() {
-    
-  // Map initialization.
-  // Uses a single map instance enabling the user to select graphically 
-  // the hatt block of choice. The map is contained within a modal and 
-  // the modal is related/bound on the show event to the calling html 
-  // container class "hatt-selection"
-
-  // this is the geojson source used in the map
-  var hattblocks = new ol.source.Vector({
-    url:HATTBLOCK_FEATURES_URL,
-    projection : 'EPSG:4326', // wgs84 ref. system
-    format: new ol.format.GeoJSON()
-  });
-  var view = new ol.View({
-      projection: 'EPSG:3857', //spherical merc 
-      center: ol.proj.transform([25,38.4],'EPSG:4326','EPSG:3857'),
-      zoom: 6,
-      maxZoom: 10
-  });
-
-  let only_blocks_layer = new ol.layer.VectorImage({
-      source: hattblocks,
-      maxZoom: 7,
-  });
-
-  let detailed_blocks_layer = new ol.layer.Vector({
-      source: hattblocks,
-      minZoom: 7,
-      declutter: true,
-      style: function (feature, resolution) {
-          labelStyle
-              .getText()
-              .setText(getHattblockLabel(feature, resolution));
-          return featureStyle;
-      },
-  });
-
-
-  var default_interactions_list = ol.interaction.defaults({altShiftDragRotate:false, pinchRotate:false});
-
-  var map = new ol.Map({
-    layers:[
-      new ol.layer.Tile({
-        source: new ol.source.OSM()
-      }),
-      only_blocks_layer,
-      detailed_blocks_layer,
-    ],
-    target: "map",
-    view: view,
-    interactions: default_interactions_list,
-  });
-
-  let selectAction = new ol.interaction.Select({
-      style: function (feature, resolution) {
-          labelStyle
-              .getText()
-              .setText(getHattblockLabel(feature, resolution));
-          return featureSelectedStyle;
-      }
-  });
-  map.addInteraction(selectAction);
-
-  // on select feature
-  selectAction.getFeatures().on('add', function (e){
-    let feature = e.element;
-    let name = feature.get('name');
-    $('#selected-feature-name').html('Επιλογή φύλλου χάρτη: ' + name);
-  });
-  // on unselect feature
-  selectAction.getFeatures().on('remove', function (e){
-    $('#selected-feature-name').html('Επιλογή φύλλου χάρτη: ');
-  });
-
-  /*
-   * Modal events
-   */
-  var bound_hatt;
-
-  $('#map-modal').on('show.bs.modal', function (e){
-    // get the related hatt container element and store it to the modal instance
-    bound_hatt = $(e.relatedTarget).parents('.hatt-selection');
-    // clear previous selected feature if exists
-    var selectedFeatures = selectAction.getFeatures();
-    selectedFeatures.clear();
-    // and select again based on related id value
-    var id = parseInt(bound_hatt.find('.hatt-id').val());  
-    if (id != NOT_SELECTED_ID){
-      if (hattblocks.getFeatures().length == 0){
-          // CAUTION: the source might not have loaded the features the first time we open the map so...
-          hattblocks.on('change', function(){
-            selectedFeatures.push(hattblocks.getFeatureById(id));
-          });
-      } else {
-        selectedFeatures.push(hattblocks.getFeatureById(id));       
-      }
-    }
-  });
-
-  $('#map-modal').on('shown.bs.modal', function (e) {
-    // BUG: this is needed because i use the map inside of a modal
-    map.updateSize();
-    // pan view to the selected feature
-    if (selectAction.getFeatures().getLength() > 0){
-      // get xmin, ymin, xmax, ymax of selected feature
-      var extent = selectAction.getFeatures().item(0).getGeometry().getExtent(); 
-      var xy = [(extent[2] + extent[0])/2, (extent[3] + extent[1])/2];
-      var pan = ol.animation.pan({
-        duration: 3*ANIM_TIME,
-        source: view.getCenter()
-      });
-      map.beforeRender(pan);
-      view.setCenter(xy);
-    } 
-  });
-
-  $('#map-modal').on('hide.bs.modal', function (e) {
-    // update the selected hatt name and hatt id elements
-    if (!bound_hatt)
-      return;
-  
-    if (selectAction.getFeatures().getLength() > 0){
-      var selectedFeature = selectAction.getFeatures().pop();
-      bound_hatt.find('.hatt-id').val(selectedFeature.getId());
-      bound_hatt.find('.hatt-name').typeahead('val', selectedFeature.get('name'));
-    } else {
-      bound_hatt.find('.hatt-id').val(NOT_SELECTED_ID);
-      bound_hatt.find('.hatt-name').typeahead('val', '');
-    }
-  });
-}
-
 function initSelectors(){
   // visibility of hatt selection html elements is based
   // on the selected srid
   // SRIDS are defined in transform.py
   
-
   function isOldGreek(srid){
     return OLD_GREEK_SRIDS.indexOf(parseInt(srid)) != -1;
   }
@@ -298,48 +132,6 @@ function initSelectors(){
   $("#input-type input").trigger('change');
 }
 
-/*
- *  Utility
- */
-function decdeg2dms(dd) {
-    let is_positive = dd >= 0;
-    dd = Math.abs(dd);
-    let seconds = dd*3600.0;
-    let minutes = Math.floor(seconds/60.0);
-    seconds = seconds % 60.0
-    let degrees = Math.floor(minutes/60.0)
-    minutes = minutes % 60.0
-    let sign = is_positive ? '' :  '-';
-    return [sign,degrees,minutes,seconds];
-}
-
-function getHattblockLabel(feature, resolution) {
-    let name = feature.get('name');
-    let okxe_id = feature.get('okxe_id');
-
-    if (resolution > 400) {
-        return [`#${okxe_id}`, ''];
-    }
-
-    let phi = feature.get('cy');
-    let lambda = feature.get('cx');
-    let [sign_phi, d_phi, m_phi, s_phi] = decdeg2dms(phi);
-    let [sign_lambda, d_lambda, m_lambda, s_lambda] = decdeg2dms(lambda);
-
-
-    let info1 = ` Φo ${sign_phi}${d_phi}\u00B0${m_phi}`;
-    let info2 = ` Λo ${sign_lambda}${d_lambda}\u00B0${m_lambda}'`;
-
-    return [
-        `#${okxe_id}`, '',
-        '\n', '',
-        ` ${name}`, '',
-        '\n', '',
-        info1, 'italic 11px Calibri, sans-serif',
-        '\n', '',
-        info2, 'italic 11px Calibri, sans-serif',
-    ];
-}
 
 function getInputType(){
   return $('#input-type input:checked').val();
@@ -376,7 +168,7 @@ function fillOutputAccuracyArea(transform_steps)
  */
 $(function() { 
   initBloodhound();
-  initMap();
+  init_map();
   initSelectors();
 
   // set hatt id element values to default -1 (nothing selected)
