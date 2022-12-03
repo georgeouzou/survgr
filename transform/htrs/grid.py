45dfe035ff46bfba696c18b389533bbe35ef9e06
georgeouzou <geothrock@gmail.com>
2022-12-03 19:11:33 +0200

    Memmap the hepos grid for reads
    Instead of reading the whole file we just memmap the grid for lazy reads
    as we most likely will read only a small fraction

--- transform/htrs/grid.py
+++ transform/htrs/grid.py
@@ -1,38 +1,33 @@
 import struct, math
-
 import numpy
 
 class GridInfo(object):
 	# rows, columns, resolution, first_cell_coords
 	header_struct = struct.Struct('I I d d d')
-	
+
 	def __init__(self, file):
-		file.seek(0) # go to beginning
-		header = file.read(GridInfo.header_struct.size)	
+		header = file.read(GridInfo.header_struct.size)
 		data = GridInfo.header_struct.unpack(header)
-		file.seek(0)
-					
+
 		self.rows = data[0] # number of rows
 		self.cols = data[1] # number of columns
 		self.res = data[2] # resolution
 		self.min_x = data[3] # minimum x value
 		self.min_y = data[4] # minimum y value
-		
+
 class GridFile(object):
 
-	def __init__(self, name, dtype='f,f'):
+	def __init__(self, name):
+		# read the header
 		with open(name,'rb') as f:
 			self.info = GridInfo(f)
-			self.dtype = dtype
-			shape = (self.info.rows, self.info.cols)
-			
-			#TODO ADD MAPPING INSTEAD OF LOADING THE WHOLE FILE
-			#self._map = numpy.memmap(f, dtype='f,f', mode='r',
-				#offset=GridInfo.header_struct.size, shape=shape)
 
-			f.seek(self.info.header_struct.size)
-			self._map = numpy.fromfile(f, dtype=self.dtype).reshape(shape)
+		assert self.info.header_struct.size == 32 # bytes
 
+		# memory map the grid for faster lazy access
+		shape = (self.info.rows, self.info.cols)
+		self._map = numpy.memmap(name, dtype='f,f', mode='r',
+			offset=self.info.header_struct.size, shape=shape)
 
 	def interpolate(self, x, y):
 		# raises IndexError if out of bounds
@@ -40,22 +35,21 @@ class GridFile(object):
 
 		norm_x = (x-self.info.min_x) / self.info.res
 		norm_y = (y-self.info.min_y) / self.info.res
-		
+
 		# rj	ul-----ur
  		# 	    |  pt  |
-		# ri	ll----lr 
+		# ri	ll----lr
 
 		ri = int(math.floor(norm_y))
 		rj = int(math.ceil(norm_y))
 		ci = int(math.floor(norm_x))
 		cj = int(math.ceil(norm_x))
-		
 
 		cell = (self._map[ri,ci], self._map[rj,ci], self._map[rj,cj], self._map[ri,cj])
-		
+
 		norm_x -= ci # normalized cell coordinates [0-1]
 		norm_y -= ri
-		
+
 		# bilinear interpolation
 		num_values = len(cell[0]) # how many values does the cell contain?
 		value = [0] * num_values
@@ -63,8 +57,8 @@ class GridFile(object):
 			# horizontal interpolations
 			vh = (cell[2][i]-cell[1][i]) * norm_x + cell[1][i]
 			vl = (cell[3][i]-cell[0][i]) * norm_x + cell[0][i]
-		
+
 			# vertical interpolation
 			value[i] =  (vh-vl) * norm_y + vl
-		
+
 		return value
