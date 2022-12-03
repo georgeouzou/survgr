3ac29575afa3ebaf3895133800b99d88db611761
georgeouzou <geothrock@gmail.com>
2022-12-03 18:04:35 +0200

    Move hepos transformer to its own file

---
+++ transform/htrs/hepos_transformer.py
@@ -0,0 +1,53 @@
+import os
+import pyproj
+from .grid import GridFile
+
+class HeposTransformer(object):
+	'''
+	Func object for HTRS07 Datum.
+	Transforms in place from HTRS / TM07 to GGRS87 / GG (inverse=False)
+	or from GGRS87 / GG to HTRS / TM07 (inverse=True)
+	'''
+	grid_path = os.path.join(os.path.dirname(__file__), "htrs07.grb")
+
+	def __init__(self, inverse):
+		# grid containing the shifts de, dn in cm
+		self._grid = GridFile(self.grid_path)
+		self._inverse = inverse
+		# extended better ggrs - htrs 7 param. transformation provided by Hepos service
+		self._htrs_to_ggrs_approx = pyproj.Transformer.from_pipeline(
+			'''
+			+proj=pipeline
+			+step +inv +proj=tmerc +lat_0=0 +lon_0=24 +k=0.9996 +x_0=500000 +y_0=-2000000 +ellps=GRS80 +units=m
+			+step +proj=cart
+			+step +proj=helmert +convention=coordinate_frame
+				+x=203.437 +y=-73.461 +z=-243.594
+				+rx=-0.170 +ry=-0.060 +rz=-0.151 +s=-0.294
+			+step +inv +proj=cart
+			+step +proj=tmerc +lat_0=0 +lon_0=24 +k=0.9996 +x_0=500000 +y_0=0 +ellps=GRS80 +units=m
+			''')
+
+	def __call__(self, x, y, z=None):
+		grid = self._grid
+		if self._inverse: #ggrs -> htrs
+			# first apply the approximate tranformation
+			h_xyz = self._htrs_to_ggrs_approx.transform(x, y, z, direction=pyproj.enums.TransformDirection.INVERSE)
+			x, y = h_xyz[0], h_xyz[1] # we need to interpolate with htrs coords
+			for index, pt in enumerate(zip(x, y)):
+				de, dn = grid.interpolate(pt[0], pt[1])
+				x[index] -= de / 100.0 # grid file contains cm corrections
+				y[index] -= dn / 100.0
+
+			return h_xyz
+
+		else: # htrs -> ggrs
+			# first apply the approximate transformation
+			g_xyz = self._htrs_to_ggrs_approx.transform(x, y, z, direction=pyproj.enums.TransformDirection.FORWARD)
+			# then apply shift correction
+			g_x, g_y = g_xyz[0], g_xyz[1]
+			for index, pt in enumerate(zip(x, y)): #again we need to interpolate with htrs coords
+				de, dn = grid.interpolate(pt[0], pt[1])
+				g_x[index] += de / 100.0
+				g_y[index] += dn / 100.0
+
+			return g_xyz
