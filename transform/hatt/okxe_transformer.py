21eeb6349bfc0ac597af86b9456993df89b9e50b
georgeouzou <geothrock@gmail.com>
2024-11-17 00:05:41 +0200

    Transform: add inverse polynomial coefficients for GGRS87 to Hatt
    This is an alternative to the iterative inverse solution.

--- transform/hatt/okxe_transformer.py
+++ transform/hatt/okxe_transformer.py
@@ -5,36 +5,56 @@ OKXETransformer transforms projected coordinates of a point
 between the Old Greek datum and the GGRS87 datum using the
 published polynomial coefficients of OKXE
 Old Greek datum:
-	epsg: (http://spatialreference.org/ref/epsg/4815/)
-	projection: Azimuthal Equidistant (Hatt map blocks)
+    epsg: (http://spatialreference.org/ref/epsg/4815/)
+    projection: Azimuthal Equidistant (Hatt map blocks)
 GGRS87 datum:
-	epsg: (http://spatialreference.org/ref/epsg/2100/html/)
-	projection: Transverse Mercator (TM87)
+    epsg: (http://spatialreference.org/ref/epsg/2100/html/)
+    projection: Transverse Mercator (TM87)
 """
 
 class OKXETransformer(object):
-	'''
-	Func object for Old Greek Datum.
-	Transforms in place from Hatt ref system to GGRS87 / GG (inverse=False)
-	or from GGRS87 / GG to Hatt ref system (inverse=True)
-	'''
-	def __init__(self, coeffs, inverse):
-		self._coeffs = coeffs
-		self._inverse = inverse
-		A = coeffs[0:6]
-		B = coeffs[6:12]
-		self._transformer = pyproj.Transformer.from_pipeline('''
-			+proj=pipeline
-			+step +proj=horner +ellps=bessel +fwd_origin=0.0,0.0 +deg=2 +range=10000000
-				+fwd_u={A0},{A1},{A3},{A2},{A5},{A4}
-				+fwd_v={B0},{B2},{B4},{B1},{B5},{B3}
-			'''.format(
-				A0=A[0], A1=A[1], A2=A[2], A3=A[3], A4=A[4], A5=A[5],
-				B0=B[0], B1=B[1], B2=B[2], B3=B[3], B4=B[4], B5=B[5]
-			))
+    '''
+    Func object for Old Greek Datum.
+    Transforms in place from Hatt ref system to GGRS87 / GG (inverse=False)
+    or from GGRS87 / GG to Hatt ref system (inverse=True)
+    '''
+    def __init__(self, coeffs, inverse, iterative_inverse=True):
+        self._coeffs = coeffs
+        self._inverse = inverse
+        A = coeffs[0:6]
+        B = coeffs[6:12]
+        if iterative_inverse:
+            self._transformer = pyproj.Transformer.from_pipeline('''
+                +proj=pipeline
+                +step +proj=horner +ellps=bessel +deg=2 +range=10000000
+                    +fwd_origin=0.0,0.0
+                    +fwd_u={A0},{A1},{A3},{A2},{A5},{A4}
+                    +fwd_v={B0},{B2},{B4},{B1},{B5},{B3}
+                '''.format(
+                    A0=A[0], A1=A[1], A2=A[2], A3=A[3], A4=A[4], A5=A[5],
+                    B0=B[0], B1=B[1], B2=B[2], B3=B[3], B4=B[4], B5=B[5],
+                ))
+        else:
+            C = coeffs[12:17]
+            D = coeffs[17:22]
+            self._transformer = pyproj.Transformer.from_pipeline('''
+                +proj=pipeline
+                +step +proj=horner +ellps=bessel +deg=2 +range=10000000
+                    +fwd_origin=0.0,0.0
+                    +fwd_u={A0},{A1},{A3},{A2},{A5},{A4}
+                    +fwd_v={B0},{B2},{B4},{B1},{B5},{B3}
+                    +inv_origin={A0},{B0}
+                    +inv_u=0.0,{C1},{C3},{C2},{C5},{C4}
+                    +inv_v=0.0,{D2},{D4},{D1},{D5},{D3}
+                '''.format(
+                    A0=A[0], A1=A[1], A2=A[2], A3=A[3], A4=A[4], A5=A[5],
+                    B0=B[0], B1=B[1], B2=B[2], B3=B[3], B4=B[4], B5=B[5],
+                    C1=C[0], C2=C[1], C3=C[2], C4=C[3], C5=C[4],
+                    D1=D[0], D2=D[1], D3=D[2], D4=D[3], D5=D[4]
+                ))
 
-	def __call__(self, x, y, z=None):
-		if (self._inverse):
-			return self._transformer.transform(x, y, z, direction=pyproj.enums.TransformDirection.INVERSE)
-		else:
-			return self._transformer.transform(x, y, z, direction=pyproj.enums.TransformDirection.FORWARD)
+    def __call__(self, x, y, z=None):
+        if (self._inverse):
+            return self._transformer.transform(x, y, z, direction=pyproj.enums.TransformDirection.INVERSE)
+        else:
+            return self._transformer.transform(x, y, z, direction=pyproj.enums.TransformDirection.FORWARD)
--- transform/hatt/tests.py
+++ transform/hatt/tests.py
@@ -6,35 +6,49 @@ from .okxe_transformer import OKXETransformer
 
 class OKXETransformTest(TestCase):
 
-	def setup(self):
-		self.coeffs = [370552.68, 0.9997155, 0.0175123, -1.08e-09, 1.63e-09, 2.04e-09,\
-		4511927.23, -0.0174755, 0.9996979, -6.50e-10, 5.60e-10, -1.65e-09]
-		self.transformer = OKXETransformer(self.coeffs, inverse=False)
-		self.inv_transformer = OKXETransformer(self.coeffs, inverse=True)
+    def setup(self):
+        self.coeffs = [370552.68, 0.9997155, 0.0175123, -1.08e-09, 1.63e-09, 2.04e-09,\
+        4511927.23, -0.0174755, 0.9996979, -6.50e-10, 5.60e-10, -1.65e-09]
+        self.transformer = OKXETransformer(self.coeffs, inverse=False)
+        self.inv_transformer = OKXETransformer(self.coeffs, inverse=True)
 
-	def test_coeff_num(self):
-		with self.assertRaises(IndexError):
-			OKXETransformer([1]*9, inverse=False)
-		with self.assertRaises(IndexError):
-			OKXETransformer([1]*8, inverse=False)
+    def test_coeff_num(self):
+        with self.assertRaises(IndexError):
+            OKXETransformer([1]*9, inverse=False)
+        with self.assertRaises(IndexError):
+            OKXETransformer([1]*8, inverse=False)
+        with self.assertRaises(IndexError):
+            OKXETransformer([1]*12, inverse=True, iterative_inverse=False)
 
-	def test_forward_func(self):
-		self.setup()
-		x1, y1 = (-16997.09, -14277.15)
-		x2, y2 = self.transformer(x1, y1)
+    def test_forward_func(self):
+        self.setup()
+        x1, y1 = (-16997.09, -14277.15)
+        x2, y2 = self.transformer(x1, y1)
 
-		self.assertEqual(round(x2,2), 353310.92)
-		self.assertEqual(round(y2,2), 4497950.95)
+        self.assertEqual(round(x2,2), 353310.92)
+        self.assertEqual(round(y2,2), 4497950.95)
 
-	def test_inverse_func(self):
-		self.setup()
-		x1, y1 = (353310.92, 4497950.95)
-		x2, y2 = self.inv_transformer(x1, y1)
+    def test_inverse_func(self):
+        self.setup()
+        x1, y1 = (353310.92, 4497950.95)
+        x2, y2 = self.inv_transformer(x1, y1)
 
-		self.assertEqual(round(x2,2),-16997.09)
-		self.assertEqual(round(y2,2),-14277.15)
-	
-
+        self.assertEqual(round(x2,2),-16997.09)
+        self.assertEqual(round(y2,2),-14277.15)
+            
+    def test_inverse_coeffs(self):
+        for i in range(Hattblock.objects.count()):
+            hb = Hattblock.objects.get(id=i+1)
+            fwd_transformer = OKXETransformer(hb.get_coeffs(), inverse=False, iterative_inverse=False)
+            x, y = (1013, 1500)
+            E, N = fwd_transformer(x, y)
+            inv_transformer = OKXETransformer(hb.get_coeffs(), inverse=True, iterative_inverse=False)
+            x1, y1 = inv_transformer(E, N)
+            
+            self.assertEqual(x, round(x1, 2))
+            self.assertEqual(y, round(y1, 2))
+            
+        
 class HattBlockTest(TestCase):
 
     def test_ids(self):
@@ -64,4 +78,8 @@ class HattBlockTest(TestCase):
         for i,j in zip(coeffs, hb.get_coeffs()):
             self.assertEqual(i, j)
 
-
+        hb = Hattblock.objects.get(name='Αλμυρός')
+        coeffs = [1.0002212,-0.0114449,6.07E-10,-9.42E-10,-5.06E-10,\
+            0.0114486,1.0002216,6.77E-10,-3.21E-10,1.70E-09]
+        for i,j in zip(coeffs, hb.get_coeffs()[12:22]):
+            self.assertEqual(i, j)
